e8cbb5ee3b9100c1a7a8aae5f655b328298a5fd5
russpetrus <russpetrus@gmail.com>
2017-02-27 13:12:32 -0500

    first commit of virtual pet project

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this first section of code and allows the user to choose an animal type from a list and name it while assigning random numbers to the other fields at instantiation
+            Random random = new Random();
+            int startHunger = random.Next(90, 100);
+            int startThirst = random.Next(60, 70);
+            int startBathroom = random.Next(50, 60);
+            int startEnergy = random.Next(60, 70);
+            int startNeediness = random.Next(75, 100);
+
+            VirtualPet userPet = new VirtualPet();//I used properties instead of constructors because the properties I created account for values less than zero and greater than 100, so everything will be handled through properties instead of constructors
+            userPet.Hunger = startHunger;
+            userPet.Thirst = startThirst;
+            userPet.BathroomUrge = startBathroom;
+            userPet.Energy = startEnergy;
+            userPet.Neediness = startNeediness;
+
+
+            Console.WriteLine("Welcome to the VirtualPet application!\nBefore we get started, please tell me your name.");
+            string ownersName = Console.ReadLine();
+            userPet.OwnersName = ownersName;
+            Console.Clear();
+
+            Console.WriteLine("Hello, {0}!\nLet's start by choosing an animal you'd like to have as a VirtualPet.\nPlease enter the number 1,2,3 that corresponds to your animal choice.", ownersName);
+
+            List<string> animalTypes = new List<string>();//using a list here instead of an array so that the choices can be easily expanded in the future
+            animalTypes.Add("penguin");
+            animalTypes.Add("monkey");
+            animalTypes.Add("dog");
+           
+            for (int i = 0; i < animalTypes.Count; i++)
+            {
+                int menuNumber = i + 1;
+                Console.WriteLine("\tPress {0} for {1}.", menuNumber, animalTypes[i]);
+            }
+
+            int animalChoice = int.Parse(Console.ReadLine());
+
+            while (animalChoice < 1 || animalChoice > animalTypes.Count)
+            {
+                Console.WriteLine("You must choose a number from the options!");
+                animalChoice = int.Parse(Console.ReadLine());
+            }
+
+
+            if (animalChoice == 1)
+            {
+                userPet.PetType = animalTypes[animalChoice - 1]; //setting the index this way will allow for expansion of the list without having to rewrite this code and accounts for the fact that the menu choice started at 1 and not 0
+                userPet.Image = userPet.PenguinImage;
+            }
+            else if (animalChoice == 2)
+            {
+                userPet.PetType = animalTypes[animalChoice - 1];
+                userPet.Image = userPet.MonkeyImage;
+            }
+            else if (animalChoice == 3)
+            {
+                userPet.PetType = animalTypes[animalChoice - 1];
+                userPet.Image = userPet.DogImage;
+            }
+            else
+            {
+                userPet.PetType = "fish";
+                Console.WriteLine("Hmm... Something must have gone wrong. It's okay, your pet will be a gerbil.");
+                userPet.Image = userPet.FishImage;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Okay, one last -- but very imporant-- question before we begin.\nWhat would you like to name your pet?");
+            userPet.PetsName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Congratulations {0}!\nYou are the proud new owner of {1} the virtual {2}!", userPet.OwnersName, userPet.PetsName, userPet.PetType);
+            Console.WriteLine(userPet.Image);
+            Console.WriteLine("Isn't {0} adorable!", userPet.PetsName);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+
+
+            //This next section of code "plays the game" until the pet's overall satisfaction reaches a certain point (here, 90). Then the game ends. The user can quit at anytime.
+            do
+            {
+
+                userPet.ShowPetStatus();
+
+                //user menu
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("\tPress 1 to feed {0}.", userPet.PetsName);
+                Console.WriteLine("\tPress 2 to give {0} water.", userPet.PetsName);
+                Console.WriteLine("\tPress 3 to take {0} outside to go potty.", userPet.PetsName);
+                Console.WriteLine("\tPress 4 to cuddle with {0}.", userPet.PetsName);
+                Console.WriteLine("\tPress 5 to play with {0}.", userPet.PetsName);
+                Console.WriteLine("\tPress 6 to quit the game.");
+                int userMethodChoice = int.Parse(Console.ReadLine());
+
+                while (userMethodChoice < 1 || userMethodChoice > 6)
+                {
+                    Console.WriteLine("You must choose an option from the menu!\nWhat would you like to do?");
+                    userMethodChoice = int.Parse(Console.ReadLine());
+                }
+                Console.Clear();
+                switch (userMethodChoice)
+                {
+
+                    case 1:
+                        {
+                            userPet.FeedPet();
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            userPet.WaterPet();
+                        }
+                        break;
+
+                    case 3:
+                        {
+                            userPet.Bathroom();
+                        }
+                        break;
+
+                    case 4:
+                        {
+                            userPet.CuddleWithPet();
+                        }
+                        break;
+
+                    case 5:
+                        {
+                            userPet.PlayWithPet();
+                        }
+                        break;
+
+                    case 6:
+                        {
+                            Console.WriteLine("{0} says, \"You're leaving me?\"", userPet.PetsName);
+                            Console.WriteLine("You're guilty of animal neglect...");
+                            Environment.Exit(0);
+                        }
+                        break;
+
+                    default: //the while loop above controls for this, but this default is an extra safe guard that will simply feed the pet. 
+                        {
+                            Console.WriteLine("{0)} says, \"Thanks for the extra food!\"", userPet.PetsName);
+                            userPet.FeedPet();
+                            break;
+                        }
+
+
+                }
+
+
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+                Console.Clear();
+
+            } while (userPet.PetSatisfaction <= 90);
+
+            //this section of code is what appears when the user has won the game (when satisfaction is greater than  or equal to 90).
+            Console.WriteLine(userPet.Image);
+            Console.WriteLine("\nCongratulations {0}!\nYou have successfully provided for {1} the {2} today!\nThe game will now close!", userPet.OwnersName, userPet.PetsName, userPet.PetType);
+            Environment.Exit(0);
+        }
+
+    }
+
+
+
+
+
+}
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        #region fields
+        const string monkeyImage = "   /~\\\n  C oo\n  _( ^)\n /   ~\\";
+        const string dogImage = " ,-.___,-.\n \\_/_ _\\_/\n  ) O_O(\n  { (_) }\n   `-^-'";
+        const string penguinImage = "   .~.\n   /V\\\n  // \\\\\n /(   )\\\n  ^`~'^";
+        const string fishImage = "}}=^=0>";
+
+        string ownersName;
+        string petsName;
+        string petType;
+        int hunger;
+        int thirst;
+        int bathroomUrge;
+        int energy;
+        int neediness;
+        string image;
+        #endregion
+
+        #region Constructors
+        public VirtualPet()
+        {
+        }
+        public VirtualPet(string ownersName, string petType, string petsName)
+        {
+            this.ownersName = ownersName;
+            this.petType = petType;
+            this.petsName = petsName;
+        }
+        public VirtualPet(int hunger, int thirst, int bathroom, int energy, int neediness)
+        {
+            this.hunger = hunger;
+            this.thirst = thirst;
+            this.bathroomUrge = bathroom;
+            this.energy = energy;
+            this.neediness = neediness;
+        }
+        #endregion
+
+        #region properties
+        public string OwnersName
+        {
+            get { return this.ownersName; }
+            set { this.ownersName = value; }
+        }
+
+        public string PetType
+        {
+            get { return this.petType; }
+            set { this.petType = value; }
+        }
+
+        public string Image
+        {
+            get { return this.image; }
+            set { this.image = value; }
+        }
+
+        public string MonkeyImage
+        {
+            get { return monkeyImage; }
+        }
+
+        public string DogImage
+        {
+            get { return dogImage; }
+        }
+
+        public string PenguinImage
+        {
+            get { return penguinImage; }
+        }
+
+        public string FishImage
+        {
+            get { return fishImage; }
+        }
+
+        public string PetsName
+        {
+            get { return this.petsName; }
+            set { this.petsName = value; }
+        }
+
+        public int Hunger
+        {
+            get
+            {
+                return this.hunger;
+
+            }
+            set
+            {
+                this.hunger = value;
+                if (this.hunger < 0)
+                {
+                    this.hunger = 0;
+                }
+                if (this.hunger > 100)
+                {
+                    this.hunger = 100;
+                }
+            }
+
+        }
+        public int Thirst
+        {
+            get { return this.thirst; }
+
+            set
+            {
+                this.thirst = value;
+                if (this.thirst < 0)
+                {
+                    this.thirst = 0;
+                }
+                if (this.thirst > 100)
+                {
+                    this.thirst = 100;
+                }
+            }
+        }
+
+        public int BathroomUrge
+        {
+            get { return this.bathroomUrge; }
+
+            set
+            {
+                this.bathroomUrge = value;
+                if (this.bathroomUrge < 0)
+                {
+                    this.bathroomUrge = 0;
+                }
+                if (this.bathroomUrge > 100)
+                {
+                    this.bathroomUrge = 100;
+                }
+            }
+        }
+
+        public int Energy
+        {
+            get { return this.energy; }
+
+            set
+            {
+                this.energy = value;
+                if (this.energy < 0)
+                {
+                    this.energy = 0;
+                }
+                if (this.energy > 100)
+                {
+                    this.energy = 100;
+                }
+            }
+        }
+        public int Neediness
+        {
+            get { return this.neediness; }
+
+            set
+            {
+                this.neediness = value;
+                if (this.neediness < 0)
+                {
+                    this.neediness = 0;
+                }
+                if (this.neediness > 100)
+                {
+                    this.neediness = 100;
+                }
+            }
+        }
+
+        public double PetSatisfaction //this property takes the average of the virtual pet's needs and subtracts it from 100 to get a sense of the pet's level of satisfaction. It will be used in main to determine the end of the game. 
+        {
+            get { return 100 - ((this.Hunger + this.Thirst + this.BathroomUrge + this.Neediness + this.Energy) / 5); }
+
+        }
+
+        #endregion
+
+        #region methods
+        public void ShowPetStatus()
+        {
+            Console.WriteLine("{0} the {1} :", this.PetsName, this.PetType);
+            Console.WriteLine("\tis {0}% hungry.", this.Hunger);
+            Console.WriteLine("\tis {0}% thirsty.", this.Thirst);
+            Console.WriteLine("\thas a {0}% need to go to the bathroom.", this.BathroomUrge);
+            Console.WriteLine("\tis {0}% energetic.", this.Energy);
+            Console.WriteLine("\tis {0}% needy.", this.Neediness);
+            Console.WriteLine();
+        }
+
+        public void FeedPet()
+        {
+            this.Hunger -= 50;
+            this.BathroomUrge += 20;
+            this.Energy += 10;
+            this.Thirst += 30;
+            Console.WriteLine(this.Image);
+            Console.WriteLine("{0} says, \"Yum!\"", this.PetsName);
+        }
+
+        public void WaterPet()
+        {
+            this.Thirst -= 80;
+            this.BathroomUrge += 20;
+            Console.WriteLine(this.Image);
+            Console.WriteLine("{0} says, \"Refreshing!\"", this.PetsName);
+        }
+
+        public void Bathroom()
+        {
+            this.BathroomUrge -= 90;
+            this.Energy -= 10;
+            Console.WriteLine(this.Image);
+            Console.WriteLine("{0} says, \"Aaaaah!\"", this.PetsName);
+        }
+
+        public void CuddleWithPet()
+        {
+            this.Energy -= 30;
+            this.Neediness -= 40;
+            this.BathroomUrge += 10;
+            Console.WriteLine(this.Image);
+            Console.WriteLine("{0} says, \"I Love you!!\"", this.PetsName);
+        }
+
+        public void PlayWithPet()
+        {
+            this.Energy -= 60;
+            this.Neediness -= 60;
+            Console.WriteLine(this.Image);
+            Console.WriteLine("{0} says, \"This is so much fun!\"", this.PetsName);
+        }
+        #endregion
+    }
+}
